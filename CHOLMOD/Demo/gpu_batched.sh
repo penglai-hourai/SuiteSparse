643f62772ffa9c022ddad34cf17902ec07d1e533
蓬莱 <penglai-hourai@outlook.com>
2017-03-22 01:00:42 -0400

    scripts fix

--- CHOLMOD/Demo/gpu_batched.sh
+++ CHOLMOD/Demo/gpu_batched.sh
@@ -3,19 +3,19 @@
 export CHOLMOD_USE_GPU=1
 
 ./cholmod_l_batched_demo \
-    /scratch/fantamon/Matrices/Emilia_923.mtx   \
-    /scratch/fantamon/Matrices/Fault_639.mtx    \
-    /scratch/fantamon/Matrices/Flan_1565.mtx    \
-    /scratch/fantamon/Matrices/G3_circuit.mtx   \
-    /scratch/fantamon/Matrices/Geo_1438.mtx     \
-    /scratch/fantamon/Matrices/Hook_1498.mtx    \
-    /scratch/fantamon/Matrices/Serena.mtx       \
-    /scratch/fantamon/Matrices/StocF-1465.mtx   \
-    /scratch/fantamon/Matrices/apache2.mtx      \
-    /scratch/fantamon/Matrices/audikw_1.mtx     \
-    /scratch/fantamon/Matrices/bone010.mtx      \
-    /scratch/fantamon/Matrices/boneS10.mtx      \
-    /scratch/fantamon/Matrices/ecology2.mtx     \
-    /scratch/fantamon/Matrices/ldoor.mtx        \
-    /scratch/fantamon/Matrices/thermal2.mtx     \
-    /scratch/fantamon/Matrices/tmt_sym.mtx      \
+    ~/Temp/Matrices/Emilia_923.mtx   \
+    ~/Temp/Matrices/Fault_639.mtx    \
+    ~/Temp/Matrices/Flan_1565.mtx    \
+    ~/Temp/Matrices/G3_circuit.mtx   \
+    ~/Temp/Matrices/Geo_1438.mtx     \
+    ~/Temp/Matrices/Hook_1498.mtx    \
+    ~/Temp/Matrices/Serena.mtx       \
+    ~/Temp/Matrices/StocF-1465.mtx   \
+    ~/Temp/Matrices/apache2.mtx      \
+    ~/Temp/Matrices/audikw_1.mtx     \
+    ~/Temp/Matrices/bone010.mtx      \
+    ~/Temp/Matrices/boneS10.mtx      \
+    ~/Temp/Matrices/ecology2.mtx     \
+    ~/Temp/Matrices/ldoor.mtx        \
+    ~/Temp/Matrices/thermal2.mtx     \
+    ~/Temp/Matrices/tmt_sym.mtx      \
