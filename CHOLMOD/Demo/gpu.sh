0bc77bcb41bdd34c2ac0670ed793ce1831115390
蓬莱 <penglai-hourai@outlook.com>
2017-04-18 15:15:57 -0400

    CHOLMOD test scripts

--- CHOLMOD/Demo/gpu.sh
+++ CHOLMOD/Demo/gpu.sh
@@ -14,7 +14,9 @@ export CHOLMOD_USE_GPU=1                            # Type      Rows        Cols
 ./cholmod_l_demo < ~/Temp/Matrices/audikw_1.mtx     # Real      943695      943695      77651847    2.1e+13
 ./cholmod_l_demo < ~/Temp/Matrices/bone010.mtx      # Real      986703      986703      47851783    2.5e+13
 ./cholmod_l_demo < ~/Temp/Matrices/boneS10.mtx      # Real      914898      914898      40878708    7.7e+11
+./cholmod_l_demo < ~/Temp/Matrices/crankseg_2.mtx   # Real
 ./cholmod_l_demo < ~/Temp/Matrices/ecology2.mtx     # Real      999999      999999      4995991     2.0e+10
 ./cholmod_l_demo < ~/Temp/Matrices/ldoor.mtx        # Real      952203      952203      42493817    1.1e+11
+./cholmod_l_demo < ~/Temp/Matrices/m_t1.mtx         # Real
 ./cholmod_l_demo < ~/Temp/Matrices/thermal2.mtx     # Real      1228045     1228045     8580313     2.8e+10
 ./cholmod_l_demo < ~/Temp/Matrices/tmt_sym.mtx      # Real      726713      726713      5080961     2.4e+10
