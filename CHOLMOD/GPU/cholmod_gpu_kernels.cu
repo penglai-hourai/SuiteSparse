e2aa8fc13f114968b4d2a0fd691b32b1bf520b50
蓬莱 <penglai-hourai@outlook.com>
2017-05-05 12:07:09 -0400

    __CUDA_ARCH__

--- CHOLMOD/GPU/cholmod_gpu_kernels.cu
+++ CHOLMOD/GPU/cholmod_gpu_kernels.cu
@@ -38,6 +38,7 @@ extern "C" {
  *    performs atomic add for type (double)
  *
  */
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600
 __device__ double atomicAdd(double* address, double val)
 {
     unsigned long long int* address_as_ull =
@@ -51,6 +52,7 @@ __device__ double atomicAdd(double* address, double val)
     } while (assumed != old);
     return __longlong_as_double(old);
 }
+#endif
 
 
 
