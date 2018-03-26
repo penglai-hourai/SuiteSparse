de69b717005e97d4870577567d2d406a1df0121b
蓬莱 <penglai-hourai@outlook.com>
2018-03-26 15:33:13 -0400

    minor corrections

--- SuiteSparse_config/SuiteSparse_config.mk
+++ SuiteSparse_config/SuiteSparse_config.mk
@@ -227,11 +227,10 @@ SUITESPARSE_VERSION = 4.5.5
         CUDA_INC      = -I$(CUDA_INC_PATH)
         NVCC          = $(CUDA_PATH)/bin/nvcc
         NVCCFLAGS     = -Xcompiler -fPIC -O3 \
-                            -gencode=arch=compute_30,code=sm_30 \
-                            -gencode=arch=compute_35,code=sm_35 \
-                            -gencode=arch=compute_50,code=sm_50 \
-                            -gencode=arch=compute_50,code=compute_50 \
-                            -gencode=arch=compute_60,code=sm_60
+                            -gencode arch=compute_30,code=sm_30 \
+                            -gencode arch=compute_35,code=sm_35 \
+                            -gencode arch=compute_50,code=sm_50 \
+                            -gencode arch=compute_60,code=sm_60
     endif
 
     #---------------------------------------------------------------------------
